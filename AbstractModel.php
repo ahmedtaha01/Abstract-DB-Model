3c8a002001b10ff2a9aeb405601883a7ede018f6
Omar AbdElwahhab <99689683+OmarAbdelwahhab30@users.noreply.github.com>
2022-03-13 20:20:53 +0200

    Update AbstractModel.php

--- AbstractModel.php
+++ AbstractModel.php
@@ -1,13 +1,17 @@
 <?php
+namespace PHPMVC\lib;
+
+use PHPMVC\LIB\Database ;
 
 class AbstractModel{
 
+    private $db ;
 
     private $Flag ;
 
     private $select = array(); 
 
-    private $query = [];
+    private $query = array() ;
 
     protected static $TableName ;
 
@@ -15,14 +19,22 @@ class AbstractModel{
 
     private $limit; 
 
-    private $insert = [];
+    private $insert ;
 
     private $update = "";
 
     private $orderBy ; 
 
     private $order ;
+
+    private $JoinInfo ;
+
     
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
     public function select()
     {
         $this->Flag = "select";
@@ -57,19 +69,27 @@ class AbstractModel{
         return $this;
     }
 
-    public function orderBy($orderby,$order = "ASC")
+    public function orderBy($orderby,$order = " ASC ")
     {
-        $this->orderBy = trim($orderby);
-        if($order != "ASC" && $order != "DESC"){
-            $order = "ASC";
-        }
-        $this->order = $order;
+        $this->orderBy = " ".trim($orderby);
+        $this->order = " ".trim($order);
         return $this;
     }
 
+
+    //actions :- 
+    // "name = omar,age = 21,address = mansoura"
+    
     public function update($actions){
         $this->Flag = "update";
         $this->update = $actions;
+        return $this;
+    }
+
+    public function join($JoinInfo)
+    {
+        $this->Flag = "join";
+        $this->JoinInfo = $JoinInfo;
         return $this;
     }
 
@@ -130,13 +150,19 @@ class AbstractModel{
 
     private function INSERT_QUERY()
     {
-        
+    
         $this->query [] = "INSERT INTO ";
         
         $this->query [] = static::$TableName ;
-        
+
+        $this->Insert_Helper();
+
+    }
+
+    public function Insert_Helper()
+    {
         $keyArray=array_keys($this->insert);
-        $columns = "( ";
+        $columns = " ( ";
         foreach($keyArray as $column){
             $columns .=  $column. (($column != end($keyArray))? ",":"");
         }
@@ -148,12 +174,11 @@ class AbstractModel{
         $ArrayValues=array_values($this->insert);
         $values = "( ";
         foreach($ArrayValues as $column){
-            $values .=  $column. (($column != end($ArrayValues))? ",":"");
+            $values .=  '"'.$column.'"'. (($column != end($ArrayValues))? ",":"");
         }
         $values .= " ) ";
 
         $this->query [] = $values ;
-
     }
 
 
@@ -173,6 +198,39 @@ class AbstractModel{
 
     }
 
+    private function JOIN_QUERY()
+    {
+        if ($this->JoinInfo['table3'] == "" ){
+            $this->query = "SELECT " .$this->JoinInfo['ColsName']. " FROM ". $this->JoinInfo['table1']."
+                                    INNER JOIN ".$this->JoinInfo['table2']." ON ".$this->JoinInfo['cond1']." WHERE "
+                                    .$this->JoinInfo['where']." ";
+            
+            
+            
+        }else{
+            //join multi tables
+
+           $this->query = "SELECT ".$this->JoinInfo['ColsName']." FROM ". $this->JoinInfo['table1']."
+                                    INNER JOIN ".$this->JoinInfo['table2']." ON ".$this->JoinInfo['cond1']."      
+                                    INNER JOIN ".$this->JoinInfo['table3']." ON ".$this->JoinInfo['cond2'].
+                                     " WHERE ".$this->JoinInfo['where']." ";
+            
+        }
+
+        if(!empty($this->order)){
+            $this->query .= "  ORDER BY  ";
+            $this->query .= $this->orderBy;
+            $this->query .= $this->order;
+        }
+
+
+        if(!empty($this->limit)){
+            $this->query .= "  LIMIT ";
+            $this->query .= $this->limit;
+        }
+        return $this->query;
+    }
+
     public function execute()
     {
         if($this->Flag == "select"){
@@ -183,9 +241,32 @@ class AbstractModel{
             $this->INSERT_QUERY();
         }elseif($this->Flag == "update"){
             $this->UPDATE_QUERY();
+        }elseif($this->Flag == "join"){
+            $this->JOIN_QUERY();
         }
-        $this->query = join(" ",$this->query);
-        return $this->query ;
+
+        if($this->Flag !="join"){
+            $this->query = join(" ",$this->query);
+        }
+
+        $this->db->query($this->query);
+        if($this->db->execute()){
+            unset($this->query);
+            $this->query = array();
+            if($this->Flag == "select" || $this->Flag =="join")
+            {
+                return $this->Fetch();
+            }else{
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function Fetch()
+    {
+        return $this->db->fetchAll();
     }
 }
+
 
